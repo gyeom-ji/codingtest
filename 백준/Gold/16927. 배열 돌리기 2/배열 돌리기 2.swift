198ef6db2d0d0f49df6eb3651aa61c7b2253e6f7
gyeom-ji <94424579+gyeom-ji@users.noreply.github.com>
2024-06-08 20:28:26 +0900

    [Gold V] Title: 배열 돌리기 2, Time: 128 ms, Memory: 69844 KB -BaekjoonHub

---
+++ 백준/Gold/16927. 배열 돌리기 2/배열 돌리기 2.swift
@@ -0,0 +1,65 @@
+let input = readLine()!.split{ $0 == " "}.map{ Int($0)!}
+let N = input[0], M = input[1], R = input[2]
+var arr = [[Int]]()
+
+for _ in 0..<N {
+    arr.append(readLine()!.split{ $0 == " "}.map{ Int($0)!})
+}
+
+rotate()
+
+for i in 0..<N {
+    for j in 0..<M {
+        print(arr[i][j], terminator: " ")
+    }
+    print()
+}
+
+func rotate() {
+    let cnt = min(M, N)/2
+    
+    for index in 0..<cnt {
+        let maxN = N - index - 1, maxM = M - index - 1
+        var rotateArr = [Int]()
+        
+        for i in index..<maxM {
+            rotateArr.append(arr[index][i])
+        }
+        
+        for i in index..<maxN {
+            rotateArr.append(arr[i][maxM])    
+        }
+        
+        for i in (index + 1...maxM).reversed() {
+            rotateArr.append(arr[maxN][i])    
+        }
+        
+        for i in (index + 1...maxN).reversed() {
+            rotateArr.append(arr[i][index])    
+        }
+        
+        let rCnt = R % rotateArr.count
+        rotateArr = Array(rotateArr[rCnt...] + rotateArr[0..<rCnt])
+        var k = 0
+        
+        for i in index..<maxM {
+            arr[index][i] = rotateArr[k]
+            k += 1
+        }
+        
+        for i in index..<maxN {
+            arr[i][maxM] = rotateArr[k]
+            k += 1
+        }
+        
+        for i in (index + 1...maxM).reversed() {
+            arr[maxN][i] = rotateArr[k]
+            k += 1
+        }
+        
+        for i in (index + 1...maxN).reversed() {
+            arr[i][index] = rotateArr[k]
+            k += 1
+        }
+    }
+}
