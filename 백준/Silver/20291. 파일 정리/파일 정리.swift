5645a19cbc670fda66c6de62d59ca103e9dcab06
gyeom-ji <94424579+gyeom-ji@users.noreply.github.com>
2024-06-10 20:51:33 +0900

    [Silver III] Title: 파일 정리, Time: 404 ms, Memory: 79728 KB -BaekjoonHub

--- 백준/Silver/20291. 파일 정리/파일 정리.swift
+++ 백준/Silver/20291. 파일 정리/파일 정리.swift
@@ -3,12 +3,7 @@ var dict = [String : Int]()
 
 for _ in 0..<N {
     let file = readLine()!.split{ $0 == "." }.map{ String($0) }
-    
-    if let exist = dict[file[1]] {
-        dict[file[1]]! += 1
-    } else {
-        dict[file[1]] = 1
-    }
+    dict[file[1], default: 0] += 1
 }
 
 for element in dict.sorted(by: {$0.key < $1.key}) {
