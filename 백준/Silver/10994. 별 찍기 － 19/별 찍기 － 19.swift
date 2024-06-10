343b31cfe50dcbd1131e0cf76df4756ad7a878f2
gyeom-ji <94424579+gyeom-ji@users.noreply.github.com>
2024-06-10 15:09:25 +0900

    [Silver IV] Title: 별 찍기 - 19, Time: 16 ms, Memory: 69232 KB -BaekjoonHub

--- 백준/Silver/10994. 별 찍기 － 19/별 찍기 － 19.swift
+++ 백준/Silver/10994. 별 찍기 － 19/별 찍기 － 19.swift
@@ -1,56 +1,21 @@
-let N = Int(readLine()!)!
+let N: Int = Int(readLine()!)!
+var stars: [String] = []
+var star: String = "*"
+var cnt = N
 
-var maxJ = 4 * (N - 1)
-var maxI = 4 * (N - 1)
-var arr = Array(repeating: Array(repeating: " ", count: maxJ + 1), count: (maxI/2) + 1)
-arr[0] = Array(repeating: "*", count: maxJ + 1)
-
-var jCnt = maxJ
-var left = 2, right = maxJ - 2
-var checkArr = Set<Int>()
-checkArr.insert(0)
-checkArr.insert(maxJ)
-
-
-if N == 1 {
-    print("*")
-} else {
-    for i in 1...maxI/2 {
-        for j in 0...maxJ {
-            if checkArr.contains(j) {
-                arr[i][j] = "*"
-            } else if i % 2 == 0 {
-                if j >= left && j <= right {
-                    arr[i][j] = "*"
-                }
-                checkArr.insert(left)
-                checkArr.insert(right)
-            }
-            if i == maxI/2 && j % 2 == 0 {
-                arr[i][j] = "*"
-            }
-        }
-        if i % 2 == 0 {
-            left += 2
-            right -= 2
-        }
-    }
-    
-    printStars()
+for i in 0..<(N - 1) {
+    stars.append(star + String(repeating: "*", count: 4 * (cnt - 1) - 1) + star)
+    stars.append(star + String(repeating: " ", count: 4 * (cnt - 1) - 1) + star)
+    star += " *"
+    cnt -= 1
 }
 
+for i in 0..<stars.count {
+    print(stars[i])
+}
 
-func printStars() {
-    for i in 0...maxI/2 {
-        for j in 0...maxJ {
-            print(arr[i][j], terminator: "")
-        }
-        print()
-    }
-    for i in (0..<maxI/2).reversed() {
-        for j in 0...maxJ {
-            print(arr[i][j], terminator: "")
-        }
-        print()
-    }
+print(String(repeating: "* ", count: 2*(N - 1)) + "*")
+
+for i in (0..<stars.count).reversed() {
+    print(stars[i])
 }
