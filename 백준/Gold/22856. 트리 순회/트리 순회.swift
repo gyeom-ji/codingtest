4d43f22d8271a6e116e2bb179ec747b860f39217
gyeom-ji <94424579+gyeom-ji@users.noreply.github.com>
2024-06-14 23:08:16 +0900

    [Gold IV] Title: 트리 순회, Time: 216 ms, Memory: 108216 KB -BaekjoonHub

--- 백준/Gold/22856. 트리 순회/트리 순회.swift
+++ 백준/Gold/22856. 트리 순회/트리 순회.swift
@@ -1,6 +1,6 @@
 let N = Int(readLine()!)!
 var moveCnt = 0
-var tree = Array(repeating: Array(repeating: -1, count: 2), count: N + 1)
+var tree = Array(repeating: (-1, -1), count: N + 1)
 var parent = Array(repeating: 1, count: N + 1)
 var visited = Array(repeating: false, count: N + 1)
 var endNode = 0
@@ -9,12 +9,11 @@ for _ in 1...N {
     let info = readLine()!.split{ $0 == " "}.map {Int(String($0))!}
     if info[1] != -1 {
         parent[info[1]] = info[0]
-        tree[info[0]][0] = info[1]
     }
     if info[2] != -1 {
         parent[info[2]] = info[0]
-        tree[info[0]][1] = info[2]
     }
+    tree[info[0]] = (info[1], info[2])
 }
 
 inorder(1)
@@ -25,20 +24,20 @@ func inorder(_ cur: Int) {
     if cur == -1 {
         return
     }
-    inorder(tree[cur][0])
+    inorder(tree[cur].0)
     endNode = cur
-    inorder(tree[cur][1])
+    inorder(tree[cur].1)
 }
 
 func similarInorder(_ cur: Int) {
-    if tree[cur][0] != -1 && !visited[tree[cur][0]] {
+    if tree[cur].0 != -1 && !visited[tree[cur].0] {
         moveCnt += 1
-        visited[tree[cur][0]] = true
-        similarInorder(tree[cur][0])
-    } else if tree[cur][1] != -1  && !visited[tree[cur][1]] {
+        visited[tree[cur].0] = true
+        similarInorder(tree[cur].0)
+    } else if tree[cur].1 != -1  && !visited[tree[cur].1] {
         moveCnt += 1
-        visited[tree[cur][1]] = true
-        similarInorder(tree[cur][1])
+        visited[tree[cur].1] = true
+        similarInorder(tree[cur].1)
     } else if cur == endNode {
         return
     } else {
