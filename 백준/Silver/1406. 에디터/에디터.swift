fb881421f0f10d0537db7080422b1133c4a8021f
gyeom-ji <94424579+gyeom-ji@users.noreply.github.com>
2024-09-29 21:35:44 +0900

    [Silver II] Title: 에디터, Time: 252 ms, Memory: 70980 KB -BaekjoonHub

---
+++ 백준/Silver/1406. 에디터/에디터.swift
@@ -0,0 +1,33 @@
+var stack = readLine()!
+let MAX = Int(readLine()!)!
+var cnt = stack.count
+var cursor = cnt
+var reverseStack = ""
+
+for _ in 0..<MAX {
+    let command = readLine()!
+    executeCommand(command)
+}
+
+func executeCommand(_ command : String){
+    switch(command.first!) {
+        case "L" :
+        if let last = stack.popLast() {
+            reverseStack.append(last)
+        }
+
+        case "D" :
+        if let last = reverseStack.popLast() {
+            stack.append(last)
+        }
+        case "B" :
+        stack.popLast()
+        case "P" :
+        stack.append(command.last!)
+        default :
+        break
+    }
+}
+
+print(stack + reverseStack.reversed())
+
