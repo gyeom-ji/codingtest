7b4eea10fc5cb2b6dba8f0c27e606e4096992c11
gyeom-ji <94424579+gyeom-ji@users.noreply.github.com>
2024-09-30 20:05:56 +0900

    Time: 1565 ms (8%), Space: 16.1 MB (72%) - LeetHub

---
+++ LeetCode/0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.swift
@@ -0,0 +1,28 @@
+class Solution {
+    func numSubarraysWithSum(_ nums: [Int], _ goal: Int) -> Int {
+        var anw = 0
+        var left = 0, right = 0
+        var sum = 0, cnt = nums.count
+
+        while left < cnt {
+            sum += nums[right]
+
+            if sum == goal {
+                anw += 1
+            } 
+            right += 1
+            if sum > goal || right == cnt {
+                sum = 0
+                left += 1
+                right = left 
+            }
+
+            // if right == cnt {
+            //     sum = 0
+            //     left += 1
+            //     right = left
+            // }
+        }
+        return anw
+    }
+}
