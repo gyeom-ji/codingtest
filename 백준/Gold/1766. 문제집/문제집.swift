48a453b5ec32e6c2970d4f9d06f755cedbfbd598
gyeom-ji <94424579+gyeom-ji@users.noreply.github.com>
2024-10-01 16:44:23 +0900

    [Gold II] Title: 문제집, Time: 696 ms, Memory: 83672 KB -BaekjoonHub

---
+++ 백준/Gold/1766. 문제집/문제집.swift
@@ -0,0 +1,90 @@
+import Foundation
+
+// 라이노님의 오픈소스  - FileIO
+final class FileIO {
+    private var buffer:[UInt8]
+    private var index: Int
+    
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        buffer = Array(fileHandle.readDataToEndOfFile())+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+        index = 0
+    }
+    
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+        
+        return buffer.withUnsafeBufferPointer { $0[index] }
+    }
+    
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+        
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45{ isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+        
+        return sum * (isPositive ? 1:-1)
+    }
+    
+    @inline(__always) func readString() -> String {
+        var str = ""
+        var now = read()
+        
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        
+        while now != 10
+                && now != 32 && now != 0 {
+            str += String(bytes: [now], encoding: .ascii)!
+            now = read()
+        }
+        
+        return str
+    }
+}
+
+let file = FileIO()
+let N = file.readInt(), M = file.readInt()
+
+var graph : [[Int]] = Array(repeating: [], count: N + 1)
+var queue = [Int]()
+var preArr = Array(repeating: 0, count: N + 1)
+var order = ""
+
+for _ in 0..<M {
+    let pre = file.readInt(), suc = file.readInt()
+    graph[pre].append(suc)
+    preArr[suc] += 1
+}
+
+for i in 1...N {
+    if preArr[i] == 0 {
+        queue.append(i)
+    }
+}
+
+bfs()
+
+print(order)
+
+func bfs() {
+    while !queue.isEmpty {
+        queue.sort(by: <)
+        let now = queue.removeFirst()
+
+        order += "\(now) "
+
+        for next in graph[now] {
+            preArr[next] -= 1
+            if preArr[next] == 0 {
+                queue.append(next)
+            }
+        }
+    }
+}
