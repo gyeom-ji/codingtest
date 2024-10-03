4ff8307edb56ee7215de73dfd8bd01a16e5b6f2f
gyeom-ji <94424579+gyeom-ji@users.noreply.github.com>
2024-10-03 19:31:40 +0900

    [Silver III] Title: N과 M (1), Time: 80 ms, Memory: 70096 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.swift
@@ -0,0 +1,17 @@
+let input = readLine()!.split{$0 == " "}.map{Int($0)!}
+var anw = ""
+
+backTracking(1, [])
+print(anw)
+
+func backTracking(_ cur: Int, _ arr: [Int]) {
+    if arr.count == input[1] {
+        anw += "\(arr.map{ String($0) }.joined(separator: " "))\n"
+        return
+    }
+    for i in 1...input[0] {
+        if !arr.contains(i) {
+            backTracking(i + 1, arr + [i])
+        }
+    }
+}
