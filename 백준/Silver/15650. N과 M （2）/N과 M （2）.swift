b08a686538a3e89e1118a5cb05e24b100bbb43e1
gyeom-ji <94424579+gyeom-ji@users.noreply.github.com>
2024-10-03 19:46:06 +0900

    [Silver III] Title: N과 M (2), Time: 8 ms, Memory: 69104 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.swift
@@ -0,0 +1,18 @@
+let input = readLine()!.split{$0 == " "}.map{Int($0)!}
+var anw = ""
+var visited = Array(repeating: false, count: input[0] + 1)
+backTracking(1, [])
+print(anw)
+
+func backTracking(_ cur: Int, _ arr: [Int]) {
+    if arr.count == input[1] {
+        anw += "\(arr.map{ String($0) }.joined(separator: " "))\n"
+        return
+    }
+    if cur > input[0] {return}
+    for i in cur...input[0] {
+        if !arr.contains(i) {
+            backTracking(i + 1, arr + [i])
+        }
+    }
+}
