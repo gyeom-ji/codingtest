e5ce6288619c217cfe167c6667f4c69543681f01
gyeom-ji <94424579+gyeom-ji@users.noreply.github.com>
2025-03-05 20:34:20 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 496 ms, Memory: 77444 KB -BaekjoonHub

---
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.swift
@@ -0,0 +1,17 @@
+let input = readLine()!.split{$0 == " "}.map{Int($0)!}
+let N = input[0], M = input[1]
+var dp = Array(repeating: Array(repeating: 0, count: N + 1), count: N + 1)
+
+for i in 1...N {
+    let arr = readLine()!.split{$0 == " "}.map{Int($0)!}
+    for j in 1...N {
+        dp[i][j] = dp[i][j - 1] + dp[i - 1][j] - dp[i - 1][j - 1] + arr[j - 1]
+    }
+}
+
+for _ in 0..<M {
+    let range = readLine()!.split{$0 == " "}.map{Int($0)!}
+    let i = range[0], j = range[1], x = range[2], y = range[3]
+    
+    print(dp[x][y] - dp[i - 1][y] - dp[x][j - 1] + dp[i - 1][j - 1])
+}
