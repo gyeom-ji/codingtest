0741f89cf96cd0b5b3ef8b71de3c6557b07e2ffd
gyeom-ji <94424579+gyeom-ji@users.noreply.github.com>
2025-03-05 22:54:46 +0900

    [Gold IV] Title: 점수따먹기, Time: 1592 ms, Memory: 69356 KB -BaekjoonHub

---
+++ 백준/Gold/1749. 점수따먹기/점수따먹기.swift
@@ -0,0 +1,22 @@
+let input = readLine()!.split{$0 == " "}.map{Int($0)!}
+var dp = Array(repeating: Array(repeating: 0, count: input[1] + 1), count: input[0] + 1)
+var result = Int.min
+
+for i in 1...input[0] {
+    let arr = readLine()!.split{$0 == " "}.map{Int($0)!}
+    
+    for j in 1...input[1] {
+        dp[i][j] = dp[i - 1][j] + dp[i][j - 1] + arr[j - 1] - dp[i - 1][j - 1]
+    }
+}
+
+for x in 1...input[0] {
+    for y in 1...input[1] {
+        for i in 1...x  {
+            for j in 1...y {
+                result = max(result, dp[x][y] - dp[i - 1][y] - dp[x][j - 1] + dp[i - 1][j - 1])
+            }
+        }
+    }
+}
+print(result)
