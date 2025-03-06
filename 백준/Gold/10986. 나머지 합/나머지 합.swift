55e3672ae02aefa91b1f9ef71f0291c6e28f6d93
gyeom-ji <94424579+gyeom-ji@users.noreply.github.com>
2025-03-06 18:31:27 +0900

    [Gold III] Title: 나머지 합, Time: 508 ms, Memory: 144224 KB -BaekjoonHub

--- 백준/Gold/10986. 나머지 합/나머지 합.swift
+++ 백준/Gold/10986. 나머지 합/나머지 합.swift
@@ -2,16 +2,17 @@ let input = readLine()!.split{$0 == " "}.map{Int($0)!}
 let arr = readLine()!.split{$0 == " "}.map{Int($0)!}
 var dp = Array(repeating:0, count: input[0] + 1)
 var count = Array(repeating:0, count: input[1])
-var result = 0
+
 
 for i in 1...input[0] {
     dp[i] = dp[i - 1] + arr[i - 1]
     count[dp[i] % input[1]] += 1
 }
 
+var result = count[0]
+
 for i in 0..<input[1] {
     result += (count[i] * (count[i] - 1)) / 2
 }
 
-print(result + count[0])
-
+print(result)
