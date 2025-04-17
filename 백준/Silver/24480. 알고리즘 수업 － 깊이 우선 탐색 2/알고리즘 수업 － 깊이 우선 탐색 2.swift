eed59c0541158a13acace5264475ad2f50e1765f
gyeom-ji <94424579+gyeom-ji@users.noreply.github.com>
2025-04-17 10:55:33 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 344 ms, Memory: 97160 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.swift
@@ -0,0 +1,30 @@
+let input = readLine()!.split{$0 == " "}.map{Int($0)!}
+var map = Array(repeating: Array(repeating: 0, count: 0), count: input[0] + 1)
+var visited = Array(repeating: false, count: input[0] + 1)
+var result = Array(repeating: 0, count: input[0])
+var cnt = 1
+
+for _ in 0..<input[1] {
+    let arr = readLine()!.split{$0 == " "}.map{Int($0)!}
+    map[arr[0]].append(arr[1])
+    map[arr[1]].append(arr[0])
+}
+
+dfs(input[2])
+
+for i in 1...input[0] where !visited[i] {
+    result[i - 1] = 0
+}
+
+print(result.map{String($0)}.joined(separator: "\n"))
+
+func dfs(_ cur: Int) {
+    visited[cur] = true
+    result[cur - 1] = cnt
+    cnt += 1
+    
+    for next in map[cur].sorted(by: >) where !visited[next] {
+        visited[next] = true
+        dfs(next)
+    }
+}
